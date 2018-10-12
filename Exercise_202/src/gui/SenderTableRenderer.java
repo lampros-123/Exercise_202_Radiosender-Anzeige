3ca4fabda917f8227ea3167daad4ceeadc645bab
lampros <matthias@futurefactory-software.com>
2018-10-12 12:52:13 +0200

    implement correct formatting for cells

--- Exercise_202/src/gui/SenderTableRenderer.java
+++ Exercise_202/src/gui/SenderTableRenderer.java
@@ -1,29 +1,47 @@
 package gui;
 
 import bl.Sender;
+import java.awt.Color;
 import java.awt.Component;
+import java.awt.Font;
 import javax.swing.JLabel;
 import javax.swing.JTable;
+import javax.swing.SwingConstants;
 import javax.swing.table.TableCellRenderer;
 
 /**
  *
  * @author Matthias
  */
-public class SenderTableRenderer implements TableCellRenderer{
+public class SenderTableRenderer implements TableCellRenderer {
 
     @Override
     public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
         Sender sender = (Sender) value;
         JLabel label = new JLabel();
         label.setOpaque(true);
-        
-        switch(column) {
-            case 0: label.setText(sender.getName()); break;
-            case 1: label.setText(String.format("%.2f", sender.getFrequency())); break;
-            case 2: label.setText(sender.getBand()); break;
+        label.setFont(new Font("Arial", Font.BOLD + Font.ITALIC, 14));
+        label.setForeground(Color.lightGray);
+        label.setHorizontalAlignment(SwingConstants.CENTER);
+        if (sender.getBand().equals("AM")) {
+            label.setBackground(Color.blue);
+        } else {
+            label.setBackground(Color.red);
         }
+
+        switch (column) {
+            case 0:
+                label.setText(sender.getName());
+                break;
+            case 1:
+                label.setText(String.format("%.2f", sender.getFrequency()));
+                break;
+            case 2:
+                label.setText(sender.getBand());
+                break;
+        }
+
         return label;
-   }
-    
+    }
+
 }
